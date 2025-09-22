d1eebd1089fa75de2e5fc82a70512edb5bdaeb20
hzm <467251648@qq.com>
2025-09-22 13:21:28 +0800

    传入颜色

--- template/utils/common.js
+++ template/utils/common.js
@@ -1,31 +1,31 @@
 export function initShader(gl, vertexSource, fragmentSource) {
-  // 创建顶点着色器对象
-  const vertexShader = gl.createShader(gl.VERTEX_SHADER); // gl.VERTEX_SHADER
-  // 创建片元着色器对象
-  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER); // gl.FRAGMENT_SHADER
+	// 创建顶点着色器对象
+	const vertexShader = gl.createShader(gl.VERTEX_SHADER); // gl.VERTEX_SHADER
+	// 创建片元着色器对象
+	const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER); // gl.FRAGMENT_SHADER
 
-  // 引入顶点、片元着色器源码
-  gl.shaderSource(vertexShader, vertexSource);
-  gl.shaderSource(fragmentShader, fragmentSource);
+	// 引入顶点、片元着色器源码
+	gl.shaderSource(vertexShader, vertexSource);
+	gl.shaderSource(fragmentShader, fragmentSource);
 
-  // 编译顶点、片元着色器
-  gl.compileShader(vertexShader);
-  gl.compileShader(fragmentShader);
+	// 编译顶点、片元着色器
+	gl.compileShader(vertexShader);
+	gl.compileShader(fragmentShader);
 
-  // 创建程序对象 program
-  const program = gl.createProgram();
+	// 创建程序对象 program
+	const program = gl.createProgram();
 
-  // 附着顶点着色器和片元着色器到 program
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
+	// 附着顶点着色器和片元着色器到 program
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragmentShader);
 
-  // 链接program
-  gl.linkProgram(program);
+	// 链接program
+	gl.linkProgram(program);
 
-  //使用
-  gl.useProgram(program);
+	//使用
+	gl.useProgram(program);
 
-  return program;
+	console.log("program is used");
+
+	return program;
 }
-
-export const theId = "123";
